9f274aba7ca09570f254c659bb7432a7b50251d9
uchunanora <uchusawai@gmail.com>
2024-01-15 20:50:27 +0900

    ♻️ update test

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <pstv/dataset.hpp>
 #include <pstv/validation.hpp>
+#include <pstv/validator_delaunay.hpp>
 #include <pstv/validator_triangulation.hpp>
 #include <utils/display.hpp>
 
 int main() {
-  std::string dir = "../data/1000/1/";
-  pstv::Dataset dataset(dir + "vert.csv", dir + "tri.csv", dir + "boundary.csv");
+  std::string dir = "./dataset/square/";
+  pstv::Dataset dataset(dir + "vertexes.csv", dir + "triangles.csv", dir + "boundaries.csv");
   if (dataset.check_overlap_vertexes()) {
     pstv::ValidatorTriangulation vt;
-    bool v = vt.validate(dataset);
+    bool is_verified_triangulation = vt.validate(dataset);
+    if (is_verified_triangulation) {
+      pstv::ValidatorDelaunay vd;
+      bool is_verified_delaunay = vd.validate(dataset, true);
+      if (!is_verified_delaunay) {
+        vd.output_new_triangles_data("./new_tri.csv");
+      }
+    }
   }
 }
